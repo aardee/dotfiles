3a15529a94bb7c18c41ec8b68186453c248f123e
R D <rajan1106@yahoo.com>
2017-12-10 14:21:29 -0500

    personalized tools

--- src/os/install/macos/my_dev_tools.sh
+++ src/os/install/macos/my_dev_tools.sh
@@ -8,6 +8,7 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 
 print_in_purple "\n   Development Tools\n\n"
 
+#brew_install "Readable Name" "Formula Name" ["TAP"] ["CMD"]  - TAP and CMD used for CASK 
 brew_install "java" "java" "caskroom/cask" "cask"
 brew_install "MongoDB" "mongo"
 brew_install "Maven" "maven"
@@ -20,12 +21,14 @@ brew_install "Gradle completions for Bash and Zsh" "gradle-completion"
 brew_install "Yarn: JavaScript package manager, replaces bower" "yarn"
 brew_install "Anaconda 2" "anaconda2" "caskroom/versions" "cask"
 
-brew_install "iterm2" "iterm2" "caskroom/versions" "cask"
-brew_install "eclipse-jee" "eclipse-jee" "caskroom/versions" "cask"
-brew_install "scala-ide" "scala-ide" "caskroom/versions" "cask"
-brew_install "textmate" "textmate" "caskroom/versions" "cask"
-brew_install "sublime-text" "sublime-text" "caskroom/versions" "cask"
+brew_install "iTerm2" "iterm2" "caskroom/versions" "cask"
+brew_install "Eclipse JEE" "eclipse-jee" "caskroom/versions" "cask"
+brew_install "Scala IDE" "scala-ide" "caskroom/versions" "cask"
+brew_install "Textmate" "textmate" "caskroom/versions" "cask"
+brew_install "Sublime Text" "sublime-text" "caskroom/versions" "cask"
 
-# Instal Docker
+brew_install "SourceTree for Git" "sourcetree" "caskroom/cask" "cask"
+
+# Install Docker
 #brew_install "Docker" "docker" "caskroom/cask" "cask"
 #brew_install "Virtualbox" "virtualbox" "caskroom/cask" "cask"
